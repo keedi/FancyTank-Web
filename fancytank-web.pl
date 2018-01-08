8e341d75ce9ec0f71d7a81e9cf693955fe446827
Keedi Kim <keedi.k@gmail.com>
2018-01-08 16:21:46 +0900

    Remove need-less perldoc plugin

--- fancytank-web.pl
+++ fancytank-web.pl
@@ -1,9 +1,6 @@
 #!/usr/bin/env perl
 use Mojolicious::Lite;
 
-# Documentation browser under "/perldoc"
-plugin 'PODRenderer';
-
 get '/' => sub {
   my $c = shift;
   $c->render(template => 'index');
