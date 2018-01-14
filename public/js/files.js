76683e595e82612f0b0ef1896d01fbbc537df681
Keedi Kim <keedi.k@gmail.com>
2018-01-14 18:52:11 +0900

    Pop up for rename action

--- public/js/files.js
+++ public/js/files.js
@@ -2,15 +2,31 @@
   $(function() {
 
     $(document).on("click", ".ft-button-popup", function (e) {
-      var filename = $(this).data('filename');
+      var action   = $(this).data("action");
+      var filename = $(this).data("filename");
       var source   = $("#ft-template").html();
       var template = Handlebars.compile(source);
-      var context  = {
-        title:        "Delete folder?",
-        body:         'Are you sure you want to delete <span class="ft-modal-filename">' + filename + '</span> from your ' + CONFIG.site_name_short + '?',
-        button_label: "Delete",
-        button_class: "ft-button-delete"
-      };
+
+      var context;
+      if (action === "delete") {
+        context  = {
+          title:        "Delete folder?",
+          body:         'Are you sure you want to delete <span class="ft-modal-filename">' + filename + '</span> from your ' + CONFIG.site_name_short + '?',
+          button_label: "Delete",
+          button_class: "ft-button-delete"
+        };
+      }
+      else if (action === "rename") {
+        context  = {
+          title:        "Rename folder?",
+          body:         'Are you sure you want to rename <span class="ft-modal-filename">' + filename + '</span> from your ' + CONFIG.site_name_short + '?',
+          button_label: "Rename",
+          button_class: "ft-button-rename"
+        };
+      }
+      else {
+        return;
+      }
       var html = template(context);
 
       $("#ft-modal-confirm").remove();
@@ -27,22 +43,22 @@
        * request to delete file on server
        */
       $.ajax({
-	url: apiUrl + "/delete",
-	type: "DELETE",
-	success: function(result) {
+        url: apiUrl + "/delete",
+        type: "DELETE",
+        success: function(result) {
           /**
            * hide from file explorer
            */
           trFile.remove();
-	},
-	error: function(jqXHR, textStatus, errorThrown) {
+        },
+        error: function(jqXHR, textStatus, errorThrown) {
           $(".ft-error-api-msg").text(jqXHR.responseJSON.Message);
           $(".ft-error-api").show(400, function() {
             setTimeout(function() {
               $(".ft-error-api").fadeOut(600);
             }, 3000);
           });
-	}
+        }
       });
 
       /**
