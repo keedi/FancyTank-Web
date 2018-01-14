c19d8be0e3c49151e0284cb4a4f2b1e34619e833
Keedi Kim <keedi.k@gmail.com>
2018-01-14 12:57:41 +0900

    Add mockup code when delete confirm button is clicked

--- public/js/files.js
+++ public/js/files.js
@@ -6,6 +6,15 @@
       $(".modal-body .ft-modal-filename").text( filename );
     });
 
+    $(document).on("click", ".ft-button-delete", function () {
+      console.log("delete button is clicked");
+      /**
+       * delete
+       */
+      // ...
+      $('#ft-modal-confirm').modal('hide');
+    });
+
   });
 }).call(this);
 
